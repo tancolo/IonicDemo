126e4b7565c438bbbcdd0415f375a33fe5f56791
shrimpcolo <shrimpcolo@gmail.com>
2015-11-16 22:41:50 +0800

    Chapter5-3 code. Adding projects.

--- www/js/app.js
+++ www/js/app.js
@@ -4,7 +4,11 @@
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
 angular.module('todo', ['ionic'])
-
+/**
+ * The Projects factory handles saving and loading projects
+ * from local storage, and also lets us save and load the
+ * last active project index.
+ */
     .run(function ($ionicPlatform) {
         $ionicPlatform.ready(function () {
             // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
@@ -17,42 +21,156 @@ angular.module('todo', ['ionic'])
             }
         });
     })
-    .controller('TodoCtrl', function ($scope, $ionicModal) { //Chapter5
-        //$scope.tasks = [
-        //    { title: 'Collect coins' },
-        //    { title: 'Eat mushrooms' },
-        //    { title: 'Get high enough to grab the flag' },
-        //    { title: 'Find the Princess' }
-        //];
-        // No need for testing data anymore
-        $scope.tasks = [];
+    .factory('Projects', function() {
+        return {
+            all: function() {
+                var projectString = window.localStorage['projects'];
+                if(projectString) {
+                    return angular.fromJson(projectString);
+                }
+                return [];
+            },
+            save: function(projects) {
+                window.localStorage['projects'] = angular.toJson(projects);
+            },
+            newProject: function(projectTitle) {
+                // Add a new project
+                return {
+                    title: projectTitle,
+                    tasks: []
+                };
+            },
+            getLastActiveIndex: function() {
+                return parseInt(window.localStorage['lastActiveProject']) || 0;
+            },
+            setLastActiveIndex: function(index) {
+                window.localStorage['lastActiveProject'] = index;
+            }
+        }
+    })
 
-        // Create and load the Modal
-        $ionicModal.fromTemplateUrl('new-task.html', function (modal) {
+    .controller('TodoCtrl', function($scope, $timeout, $ionicModal, Projects, $ionicSideMenuDelegate) {
+
+        // A utility function for creating a new project
+        // with the given projectTitle
+        var createProject = function(projectTitle) {
+            var newProject = Projects.newProject(projectTitle);
+            $scope.projects.push(newProject);
+            Projects.save($scope.projects);
+            $scope.selectProject(newProject, $scope.projects.length-1);
+        }
+
+
+        // Load or initialize projects
+        $scope.projects = Projects.all();
+
+        // Grab the last active, or the first project
+        $scope.activeProject = $scope.projects[Projects.getLastActiveIndex()];
+
+        // Called to create a new project
+        $scope.newProject = function() {
+            var projectTitle = prompt('Project name');
+            if(projectTitle) {
+                createProject(projectTitle);
+            }
+        };
+
+        // Called to select the given project
+        $scope.selectProject = function(project, index) {
+            $scope.activeProject = project;
+            Projects.setLastActiveIndex(index);
+            $ionicSideMenuDelegate.toggleLeft(false);
+        };
+
+        // Create our modal
+        $ionicModal.fromTemplateUrl('new-task.html', function(modal) {
             $scope.taskModal = modal;
         }, {
-            scope: $scope,
-            animation: 'slide-in-up'
+            scope: $scope
         });
 
-
-        // Called when the form is submitted
-        $scope.createTask = function (task) {
-            $scope.tasks.push({
+        $scope.createTask = function(task) {
+            if(!$scope.activeProject || !task) {
+                return;
+            }
+            $scope.activeProject.tasks.push({
                 title: task.title
             });
             $scope.taskModal.hide();
+
+            // Inefficient, but save all the projects
+            Projects.save($scope.projects);
+
             task.title = "";
         };
 
-        // Open our new task modal
-        $scope.newTask = function () {
+        $scope.newTask = function() {
             $scope.taskModal.show();
         };
 
-        // Close the new task modal
-        $scope.closeNewTask = function () {
+        $scope.closeNewTask = function() {
             $scope.taskModal.hide();
+        }
+
+        $scope.toggleProjects = function() {
+            $ionicSideMenuDelegate.toggleLeft();
         };
 
+
+        // Try to create the first project, make sure to defer
+        // this by using $timeout so everything is initialized
+        // properly
+        $timeout(function() {
+            if($scope.projects.length == 0) {
+                while(true) {
+                    var projectTitle = prompt('Your first project title:');
+                    if(projectTitle) {
+                        createProject(projectTitle);
+                        break;
+                    }
+                }
+            }
+        });
+
     });
+
+    //old controller
+    //.controller('TodoCtrl', function ($scope, $ionicModal) { //Chapter5
+    //    //$scope.tasks = [
+    //    //    { title: 'Collect coins' },
+    //    //    { title: 'Eat mushrooms' },
+    //    //    { title: 'Get high enough to grab the flag' },
+    //    //    { title: 'Find the Princess' }
+    //    //];
+    //    // No need for testing data anymore
+    //    $scope.tasks = [];
+    //
+    //    // Create and load the Modal
+    //    $ionicModal.fromTemplateUrl('new-task.html', function (modal) {
+    //        $scope.taskModal = modal;
+    //    }, {
+    //        scope: $scope,
+    //        animation: 'slide-in-up'
+    //    });
+    //
+    //
+    //    // Called when the form is submitted
+    //    $scope.createTask = function (task) {
+    //        $scope.tasks.push({
+    //            title: task.title
+    //        });
+    //        $scope.taskModal.hide();
+    //        task.title = "";
+    //    };
+    //
+    //    // Open our new task modal
+    //    $scope.newTask = function () {
+    //        $scope.taskModal.show();
+    //    };
+    //
+    //    // Close the new task modal
+    //    $scope.closeNewTask = function () {
+    //        $scope.taskModal.hide();
+    //    };
+    //
+    //});
