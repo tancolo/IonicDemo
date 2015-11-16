56141b3eae8046e8a294dcbb078ac6066316059c
shrimpcolo <shrimpcolo@gmail.com>
2015-11-16 21:57:35 +0800

    Chapter5-2 code. Creating tasks.

--- www/js/app.js
+++ www/js/app.js
@@ -17,11 +17,42 @@ angular.module('todo', ['ionic'])
             }
         });
     })
-    .controller('TodoCtrl', function($scope) { //Chapter5
-        $scope.tasks = [
-            { title: 'Collect coins' },
-            { title: 'Eat mushrooms' },
-            { title: 'Get high enough to grab the flag' },
-            { title: 'Find the Princess' }
-        ];
+    .controller('TodoCtrl', function ($scope, $ionicModal) { //Chapter5
+        //$scope.tasks = [
+        //    { title: 'Collect coins' },
+        //    { title: 'Eat mushrooms' },
+        //    { title: 'Get high enough to grab the flag' },
+        //    { title: 'Find the Princess' }
+        //];
+        // No need for testing data anymore
+        $scope.tasks = [];
+
+        // Create and load the Modal
+        $ionicModal.fromTemplateUrl('new-task.html', function (modal) {
+            $scope.taskModal = modal;
+        }, {
+            scope: $scope,
+            animation: 'slide-in-up'
+        });
+
+
+        // Called when the form is submitted
+        $scope.createTask = function (task) {
+            $scope.tasks.push({
+                title: task.title
+            });
+            $scope.taskModal.hide();
+            task.title = "";
+        };
+
+        // Open our new task modal
+        $scope.newTask = function () {
+            $scope.taskModal.show();
+        };
+
+        // Close the new task modal
+        $scope.closeNewTask = function () {
+            $scope.taskModal.hide();
+        };
+
     });
